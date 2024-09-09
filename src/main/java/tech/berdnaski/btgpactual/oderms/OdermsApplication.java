1ee090b341100723219f5a304903ce7f1582d61d
berdnaski <berdnaski1177@gmail.com>
2024-09-09 10:38:45 -0300

    chore: configuration rabbit, docker and mongodb

---
+++ src/main/java/tech/berdnaski/btgpactual/oderms/OdermsApplication.java
@@ -0,0 +1,13 @@
+package tech.berdnaski.btgpactual.oderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdermsApplication.class, args);
+	}
+
+}
