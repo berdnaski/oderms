a269a5a287b007f71b1b67ab930fed907c1e506f
berdnaski <berdnaski1177@gmail.com>
2024-09-10 09:31:57 -0300

    chore: modifications OrderCreatedListener

--- src/main/java/tech/berdnaski/btgpactual/oderms/listener/OrderCreatedListener.java
+++ src/main/java/tech/berdnaski/btgpactual/oderms/listener/OrderCreatedListener.java
@@ -7,14 +7,23 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import tech.berdnaski.btgpactual.oderms.config.RabbitMqConfig;
 import tech.berdnaski.btgpactual.oderms.listener.dto.OrderCreatedEvent;
+import tech.berdnaski.btgpactual.oderms.service.OrderService;
 
 @Component
 public class OrderCreatedListener {
 
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
 
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @RabbitListener(queues = RabbitMqConfig.ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
         logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
     }
 }
